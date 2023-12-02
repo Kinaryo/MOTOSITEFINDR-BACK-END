644005c635bb8697f8bec7db9ea398d0eddef810
Kinaryo <kinaryo733huda@gmail.com>
2023-12-02 19:57:45 +0700

    perbaikan mongodb

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const ejsMate = require('ejs-mate')
+const ejsMate = require('ejs-mate');
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session')
-const flash = require('connect-flash')
-const methodOverride = require('method-override')
-const mongoose = require('mongoose')
-const path = require ('path')
-const app = express();
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
+const session = require('express-session');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const path = require('path');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user'); // Pastikan Anda mengimpor model User
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 mongoose.set('strictQuery', false);
 
+// Fungsi untuk menghubungkan ke MongoDB
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
@@ -27,88 +29,66 @@ const connectDB = async () => {
     }
 };
 
+// Konfigurasi view engine dan middleware
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-
-// models
-const User =require('./models/user')
-// connect to mngodb
-mongoose.connect('mongodb://127.0.0.1/motositefinder')
-.then((result)=>{
-    console.log('connected to mongodb')
-}).catch((err)=>{
-    console.log(err)
-})
-
-app.engine('ejs',ejsMate)
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'))
-
-// middleware untuk mendapatkan data dari body 
-app.use(express.urlencoded({extended:true}));
-// mengubahmethod post menjadi method yang akan di pakai melalui query 
-app.use(methodOverride('_method'))
-// folder public untuk file static 
-app.use(express.static(path.join(__dirname,'public')))
-// inisialisasi session 
-app.use(session(
-    {
-        secret: 'this-is-a-secret-key',
-        resave: false,
-        saveUnitialized : false,
-        cookie:{
-            httpOnly: true,
-            expires: Date.now()+1000*60*60*24*7,
-            maxAge: 100*60*60*24*27
-        }
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'this-is-a-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 100 * 60 * 60 * 24 * 27
     }
-
-))
+}));
 app.use(flash());
-
 app.use(passport.initialize());
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+app.use(passport.session());
 
+// Passport Configuration
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-
-app.use((req,res,next)=>{
-    // pengkondisian logout
+// Global middleware untuk data pengguna
+app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg')
+    res.locals.error_msg = req.flash('error_msg');
     next();
-})
+});
 
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+app.use('/', require('./routes/auth'));
+app.use('/pages', require('./routes/motor'));
+app.use('/pages/:motor_id/comments', require('./routes/comment'));
 
-app.use('/',require('./routes/auth') )
-app.use('/pages',require('./routes/motor'))
-app.use('/pages/:motor_id/comments',require('./routes/comment'))
+// Handling error sementara
+app.all('*', (req, res, next) => {
+    next(new ErrorHandler('Page not Found', 404));
+});
 
+// Middleware untuk menangani error
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = "Oh no, something went wrong";
+    res.status(statusCode).render('error', { err });
+});
 
-//melihat eror sementara 
-app.all('*',(req,res,next)=>{
-    next(new ErrorHandler('Page not Faund',404))
-})
-
-// middleware untuk menangani suatu error
-app.use((err,req,res,next)=>{
-    const {statusCode = 500 } = err;
-    if(!err.message) err.message = "oh no, Something went wrong"
-    res.status(statusCode).render('error',{err})
-
-})
-app.listen(5000,()=>{
-    console.log(`server is running on http://127.0.0.1:5000`)
-})
-
+// Menghubungkan ke MongoDB dan menjalankan server
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`Listening On Port ${PORT}`);
+        console.log(`Server is running on http://127.0.0.1:${PORT}`);
     });
 });
