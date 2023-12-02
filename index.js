80a6159585a228dbfba3e97f11e2ef06afba108d
Kinaryo <137769524+Kinaryo@users.noreply.github.com>
2023-12-03 04:44:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const connectDB = async () => {
 const Motor = require('./models/motor');
 const Comment = require('./models/comment');
 const { motorSchema } = require('./schemas/motor');
-const { commentSchema} = require('.schemas/comment'
+const { commentSchema } = require('./schemas/comment'); // Fixed import path
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -47,27 +47,26 @@ const validateMotor = (req, res, next) => {
     }
 };
 
-const validateComment = (req,res,next)=>{
-    const {error} = commentSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        console.log(error)
-        return next(new ErrorHandler(error.details[0].message,400))
-    }else{
-        next()
+const validateComment = (req, res, next) => {
+    const { error } = commentSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        console.log(error);
+        return next(new ErrorHandler(msg, 400));
+    } else {
+        next();
     }
-}
+};
 
+// Server route/landing page
+app.get('/', (req, res) => {
+    res.send('Server Motositefinder');
+});
 
-// server route/landing page 
-app.get('/',(req,res)=>{
-    res.send('Server Motositefinder')
-})
-
-// halaman home
-app.get('/',(req,res)=>{
-    res.send('home')
-})
+// Halaman home
+app.get('/home', (req, res) => {
+    res.send('home');
+});
 
 // Endpoint untuk mendapatkan semua data motor dalam bentuk JSON
 app.get('/allMotors', wrapAsync(async (req, res) => {
@@ -82,22 +81,24 @@ app.get('/motors/:id', wrapAsync(async (req, res) => {
     res.json({ motor });
 }));
 
-app.get('/create/form', (req,res)=>{
-    res.json({message: 'Halaman edit', motor})
-})
-// menambahkan data motor baru dalam bentuk JSON
+app.get('/create/form', (req, res) => {
+    res.json({ message: 'Halaman edit', motor });
+});
+
+// Menambahkan data motor baru dalam bentuk JSON
 app.post('/create/form/motor', validateMotor, wrapAsync(async (req, res, next) => {
     const motor = new Motor(req.body.motor);
     await motor.save();
     res.json({ message: 'Motor added successfully', motor });
 }));
 
-// menuju ke halaman edit 
-app.get('/motors/:id/edit',wrapAsync(async(req,res)=>{
+// Menuju ke halaman edit
+app.get('/motors/:id/edit', wrapAsync(async (req, res) => {
     const motor = await Motor.findById(req.params.id);
-    res.json({message: 'Halaman edit', motor})
-}))
-// mengupdate data motor berdasarkan ID dalam bentuk JSON
+    res.json({ message: 'Halaman edit', motor });
+}));
+
+// Mengupdate data motor berdasarkan ID dalam bentuk JSON
 app.put('/motors/:id/edit/update', validateMotor, wrapAsync(async (req, res) => {
     const { id } = req.params;
     const motor = await Motor.findByIdAndUpdate(id, { ...req.body.motor });
@@ -110,30 +111,26 @@ app.delete('/motors/:id', wrapAsync(async (req, res) => {
     res.json({ message: 'Motor deleted successfully' });
 }));
 
-
-
-
-//  routes komentar
-app.post('motors/:id/comments',validateComment, wrapAsync(async( req,res)=>{
+// Routes komentar
+app.post('/motors/:id/comments', validateComment, wrapAsync(async (req, res) => {
     const comment = new Comment(req.body.comment);
     const motor = await Motor.findById(req.params.id);
     motor.comments.push(comment);
     await comment.save();
-    await motor.save()
-    res.json({'succes add comment',motor})
+    await motor.save();
+    res.json({ message: 'Success add comment', motor });
+}));
 
-}))
-app.all('*',(req,res,next)=>{
-    next(new ErrorHandler('Page not Faund',404))
-})
+app.all('*', (req, res, next) => {
+    next(new ErrorHandler('Page not Found', 404));
+});
 
-// middleware untuk menangani suatu error
-app.use((err,req,res,next)=>{
-    const {statusCode = 500 } = err;
-    if(!err.message) err.message = "oh no, Something went wrong"
-    res.status(statusCode).render('error',{err})
-
-})
+// Middleware untuk menangani suatu error
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh no, something went wrong';
+    res.status(statusCode).render('error', { err });
+});
 
 connectDB().then(() => {
     app.listen(PORT, () => {
