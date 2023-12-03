3ea1da64f5ccfa3085637875f9388cc7145315c9
Kinaryo <kinaryo733huda@gmail.com>
2023-12-03 11:01:19 +0700

    controllers dan validasi input

--- routes/comment.js
+++ routes/comment.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const wrapAsync = require('../utils/wrapAsync')
+const controllersComment = require('../controllers/comment')
 const isValidObjectId = require('../middlewares/isValidObjectId')
 const ErrorHandler = require('../utils/ErrorHandler')
 const isAuth = require('../middlewares/isAuth')
 const {isAuthorComment} = require('../middlewares/isAuthor')
+const {validateComment} = require('../middlewares/validator')
 //modelss
 const Motor = require('../models/motor')
 const Comment = require('../models/comment')
@@ -12,39 +14,9 @@ const {commentSchema} = require('../schemas/comment')
 
 const router = express.Router({mergeParams : true});
 
-const validateComment = (req,res,next)=>{
-    const {error} = commentSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        console.log(error)
-        return next(new ErrorHandler(error.details[0].message,400))
-    }else{
-        next()
-    }
-}
-
 //bagian komentar
-router.post('/',isAuth, isValidObjectId('/motors'),validateComment, wrapAsync(async (req, res) => {
-   
-    const {motor_id} = req.params
-    const comment = new Comment(req.body.comment);
-    comment.author = req.user._id;
-    await comment.save();
-
-    const motor = await Motor.findById(motor_id);
-    motor.comments.push(comment);
-    await motor.save()
-    const msg = req.flash('success_msg','anda berhasil menambahkan komentar')
-    res.json({ message: 'Success add comment', motor });
-}));
-
+router.post('/',isAuth, isValidObjectId('/motors'),validateComment, wrapAsync(controllersComment.store));
 // menghapus komentar 
-router.delete('/motors/:motor_id/comments/:comment_id', isAuth,isAuthorComment,isValidObjectId('/motors'),wrapAsync(async (req, res) => {
-    const { motor_id, comment_id } = req.params;
-    await Motor.findByIdAndUpdate(motor_id, { $pull: { comments: { _id: comment_id } } });
-    await Comment.findByIdAndDelete(comment_id);
-    const msg = req.flash('success_msg','comment berhasil dihapus')
-    res.json({message: `Success Delete Comment`, motor})
-}));
+router.delete('/motors/:motor_id/comments/:comment_id', isAuth,isAuthorComment,isValidObjectId('/motors'),wrapAsync(controllersComment.destroy));
 
 module.exports= router;
--- routes/motor.js
+++ routes/motor.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const wrapAsync = require('../utils/wrapAsync');
+const constrollersMotor = require('../controllers/motor')
 const ErrorHandler = require('../utils/ErrorHandler');
 const isValidObjectId = require('../middlewares/isValidObjectId')
 const isAuth = require('../middlewares/isAuth')
 const isAuthor = require('../middlewares/isAuthor')
+const {isAuthorMotor}  = require('../middlewares/isAuthor')
+const {validateMotor} = require('../middlewares/validator')
 // model
 const Motor = require('../models/motor');
 const router = express.Router();
@@ -12,98 +15,20 @@ const { motorSchema } = require('../schemas/motor');
 
 
 
-const validateMotor = (req, res, next) => {
-    const { error } = motorSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        console.log(error);
-        return next(new ErrorHandler(error.details[0].message, 400));
-    } else {
-        next();
-    }
-};
-
-
-function escapeRegex(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-  }
-router.get('/Searchpages', wrapAsync(async (req, res) => {
-      let motors;
-
-      // Handling search
-      if (req.query.search) {
-        const searchRegex = new RegExp(escapeRegex(req.query.search), 'gi');
-        motors = await Motor.find({ title: searchRegex });
-      } else {
-        // Handling filter
-        if (req.query.sortBy === 'terbaru') {
-          motors = await Motor.find().sort({ dateTime: -1 });
-        } else if (req.query.sortBy === 'terlama') {
-          motors = await Motor.find().sort({ dateTime: 1 });
-        } else {
-          motors = await Motor.find();
-        }
-      }
-    //   Mengirim data sebagai JSON
-      res.json({ motors });
-    })
-  );
-
-// Endpoint untuk mendapatkan semua data motor dalam bentuk JSON
-router.get('/', wrapAsync(async (req, res) => {
-  const {id} = req.params
-  const motors = await Motor.find(id)
-  const msg = req.flash('succes_msg','motor fetched successfully')
-   res.json({msg, motors });
-}));
-
+router.get('/Searchpages', wrapAsync(constrollersMotor.search));
+//  mendapatkan semua data motor dalam bentuk JSON
+router.get('/', wrapAsync(constrollersMotor.index));
 // mendapatkan detail motor berdasarkan ID dalam bentuk JSON
-router.get('/:id',isValidObjectId('/motors'),wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const motor = await Motor.findById(id)
-    .populate({
-      path : 'comments',
-      populate:{
-        path:'author'
-      }
-    })
-    .populate('author')
-console.log(motor)
-    res.json({ motor });
-}));
-
-router.get('/create/form', isAuth, (req, res) => {
-    res.json({ message: 'Halaman edit' });
-});
-
+router.get('/:id',isValidObjectId('/motors'),wrapAsync(constrollersMotor.detail));
+// menuju halaman new data motor 
+router.get('/create/form', isAuth, constrollersMotor.form ) 
 // Menambahkan data motor baru dalam bentuk JSON
-router.post('/create/form/motor',isAuth, validateMotor, wrapAsync(async (req, res) => {
-    const motor = new Motor(req.body.motor);
-    motor.author = req.user._id;
-    await motor.save();
-    req.flash('success_msg','Selamat, anda berhasil menambahkan data')
-    res.json({ message: 'Motor added successfully', motor });
-}));
-
-// Menuju ke halaman edit
-router.get('/:id/edit',isAuth,isAuthor.isAuthorMotor, isValidObjectId('/motors'),wrapAsync(async (req, res) => {
-    const motor = await Motor.findById(req.params.id);
-    res.json({ message: 'Halaman edit', motor });
-}));
-
+router.post('/create/form/motor',isAuth, validateMotor, wrapAsync(constrollersMotor.store));
+// masuk halaman edit
+router.get('/:id/edit',isAuth,isAuthorMotor, isValidObjectId('/motors'),wrapAsync(constrollersMotor.edit))
 // Mengupdate data motor berdasarkan ID dalam bentuk JSON
-router.put('/:id/edit/update',isAuth,isAuthor.isAuthorMotor,isValidObjectId('/motors'), validateMotor, wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const motor = await Motor.findByIdAndUpdate(id, { ...req.body.motor });
-    req.flash('success_msg','Anda berhasil meng-update data');
-    res.json({ message: 'Motor updated successfully', motor });
-}));
-
+router.put('/:id/edit/update',isAuth,isAuthorMotor,isValidObjectId('/motors'), validateMotor, wrapAsync(constrollersMotor.update));
 // menghapus data motor berdasarkan ID dalam bentuk JSON
-router.delete('/:id',isAuth,isAuthor.isAuthorMotor,isValidObjectId('/motors'), wrapAsync(async (req, res) => {
-    await Motor.findByIdAndDelete(req.params.id);
-    const msg = req.flash('success_msg','Data berhasil dihapus');
-    res.json({ message: 'Motor deleted successfully' });
-}));
+router.delete('/:id',isAuth,isAuthorMotor,isValidObjectId('/motors'), wrapAsync(constrollersMotor.destroy));
 
 module.exports = router;
