08295c3bc6580ea868a9fff3804bc91a83253d3d
Kinaryo <kinaryo733huda@gmail.com>
2023-12-04 04:45:39 +0700

    memperbaiki cors

--- models/user.js
+++ models/user.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
-const passportLocalMongoose = require(`passport-local-mongoose`)
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true,
-    }
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+});
 
-})
-userSchema.plugin(passportLocalMongoose)
-module.exports = mongoose.model('User',userSchema)
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', userSchema);
